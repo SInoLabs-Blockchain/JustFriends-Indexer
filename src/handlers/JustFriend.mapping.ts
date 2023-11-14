f1acfcc9d28298fe4c37fa1717991bce049ec089
anhnn <anhngoc272408@gmail.com>
2023-11-14 21:08:15 +0700

    commit

--- src/handlers/JustFriend.mapping.ts
+++ src/handlers/JustFriend.mapping.ts
@@ -56,6 +56,7 @@ export function handleUpvoted(event: Upvoted): void {
   entity.hash = event.params.hash.toHexString();
   entity.account = event.params.account.toHexString();
   entity.type = true;
+  entity.timestamp = Date.now().toString();
   entity.save();
 }
 
