6dcf6fb4c7f16430dfc22c90fc9ffc15d0c5397c
anhnn272408 <anhngoc.ng27@gmail.com>
2023-11-18 12:40:29 +0700

    commit

--- src/handlers/JustFriend.mapping.ts
+++ src/handlers/JustFriend.mapping.ts
@@ -20,20 +20,20 @@ export function handleContentCreated(event: ContentCreated): void {
     event.params.creator.toHexString(),
   ]);
   const id = event.transaction.hash.toHex();
-  var contentEntity = new ContentEntity(id);
-  contentEntity.hash = event.params.hash.toHexString();
-  contentEntity.creator = event.params.creator.toHexString();
-  contentEntity.startedPrice = event.params.startedPrice;
-  contentEntity.timestamp = event.block.timestamp;
-  contentEntity.isPaid = event.params.isPaid;
-  contentEntity.save();
-  var creatorEntity = CreatorEntity.load(event.params.creator.toHexString());
-  if (creatorEntity == null) {
-    creatorEntity = new CreatorEntity(event.params.creator.toHexString());
-    creatorEntity.address = event.params.creator.toHexString();
-    creatorEntity.totalUpVote = new BigInt(0);
-    creatorEntity.totalDownVote = new BigInt(0);
-    creatorEntity.creditScore = new BigInt(0);
+  var contentEnt = new ContentEntity(id);
+  contentEnt.hash = event.params.hash.toHexString();
+  contentEnt.creator = event.params.creator.toHexString();
+  contentEnt.startedPrice = event.params.startedPrice;
+  contentEnt.timestamp = event.block.timestamp;
+  contentEnt.isPaid = event.params.isPaid;
+  contentEnt.save();
+  var creatorEnt = CreatorEntity.load(event.params.creator.toHexString());
+  if (creatorEnt == null) {
+    creatorEnt = new CreatorEntity(event.params.creator.toHexString());
+    creatorEnt.address = event.params.creator.toHexString();
+    creatorEnt.totalUpVote = new BigInt(0);
+    creatorEnt.totalDownVote = new BigInt(0);
+    creatorEnt.creditScore = new BigInt(0);
   }
   var userPostEnt = new UserPostEntity(
     event.params.hash.toHexString() + "-" + event.params.creator.toHexString()
@@ -47,12 +47,12 @@ export function handleContentCreated(event: ContentCreated): void {
 export function handleAccessPurchased(event: AccessPurchased): void {
   log.info("Event AccessPurchased: hash={}", [event.params.hash.toHexString()]);
   const hash = event.transaction.hash.toHex();
-  var entity = new AccessPurchasedEntity(hash);
-  entity.hash = event.params.hash.toHexString();
-  entity.buyer = event.params.buyer.toHexString();
-  entity.amount = event.params.amount;
-  entity.totalPrice = event.params.totalPrice;
-  entity.save();
+  var apEnt = new AccessPurchasedEntity(hash);
+  apEnt.hash = event.params.hash.toHexString();
+  apEnt.buyer = event.params.buyer.toHexString();
+  apEnt.amount = event.params.amount;
+  apEnt.totalPrice = event.params.totalPrice;
+  apEnt.save();
 
   var userPostEnt = new UserPostEntity(
     event.params.hash.toHexString() + "-" + event.params.buyer.toHexString()
@@ -66,12 +66,12 @@ export function handleAccessPurchased(event: AccessPurchased): void {
 export function handleAccessSold(event: AccessSold): void {
   log.info("Event AccessSold: hash={}", [event.params.hash.toHexString()]);
   const hash = event.transaction.hash.toHex();
-  var entity = new AccessSoldEntity(hash);
-  entity.hash = event.params.hash.toHexString();
-  entity.seller = event.params.seller.toHexString();
-  entity.amount = event.params.amount;
-  entity.totalPrice = event.params.totalPrice;
-  entity.save();
+  var asEnt = new AccessSoldEntity(hash);
+  asEnt.hash = event.params.hash.toHexString();
+  asEnt.seller = event.params.seller.toHexString();
+  asEnt.amount = event.params.amount;
+  asEnt.totalPrice = event.params.totalPrice;
+  asEnt.save();
   const userPostId = event.params.hash.toHexString() + "-" + event.params.seller.toHexString();
   var userPostEnt = UserPostEntity.load(userPostId);
   if(!userPostEnt?.isOwner){
@@ -85,41 +85,41 @@ export function handleAccessSold(event: AccessSold): void {
 export function handleUpvoted(event: Upvoted): void {
   log.info("Event UpVoted: hash={}", [event.params.hash.toHexString()]);
   const hash = event.transaction.hash.toHex();
-  var entity = new VotedEntity(hash);
-  entity.hash = event.params.hash.toHexString();
-  entity.account = event.params.account.toHexString();
-  entity.type = true;
-  entity.timestamp = event.block.timestamp;
-  entity.save();
+  var votedEnt = new VotedEntity(hash);
+  votedEnt.hash = event.params.hash.toHexString();
+  votedEnt.account = event.params.account.toHexString();
+  votedEnt.type = true;
+  votedEnt.timestamp = event.block.timestamp;
+  votedEnt.save();
   const coefficientUp = new BigInt(10);
   const coefficientDown = new BigInt(8);
-  var creatorEntity = CreatorEntity.load(event.params.creator.toHexString());
-  if (creatorEntity != null) {
-    creatorEntity.totalUpVote = creatorEntity.totalUpVote.plus(new BigInt(1));
-    creatorEntity.creditScore = creatorEntity.totalUpVote
+  var creatorEnt = CreatorEntity.load(event.params.creator.toHexString());
+  if (creatorEnt != null) {
+    creatorEnt.totalUpVote = creatorEnt.totalUpVote.plus(new BigInt(1));
+    creatorEnt.creditScore = creatorEnt.totalUpVote
       .times(coefficientUp)
-      .minus(creatorEntity.totalDownVote.times(coefficientDown));
+      .minus(creatorEnt.totalDownVote.times(coefficientDown));
   }
 }
 
 export function handleDownvoted(event: Downvoted): void {
   log.info("Event DownVoted: hash={}", [event.params.hash.toHexString()]);
   const hash = event.transaction.hash.toHex();
-  var entity = new VotedEntity(hash);
-  entity.hash = event.params.hash.toHexString();
-  entity.account = event.params.account.toHexString();
-  entity.type = false;
-  entity.timestamp = event.block.timestamp;
-  entity.save();
+  var votedEnt = new VotedEntity(hash);
+  votedEnt.hash = event.params.hash.toHexString();
+  votedEnt.account = event.params.account.toHexString();
+  votedEnt.type = false;
+  votedEnt.timestamp = event.block.timestamp;
+  votedEnt.save();
   const coefficientUp = new BigInt(10);
   const coefficientDown = new BigInt(8);
-  var creatorEntity = CreatorEntity.load(event.params.creator.toHexString());
-  if (creatorEntity != null) {
-    creatorEntity.totalDownVote = creatorEntity.totalDownVote.plus(
+  var creatorEnt = CreatorEntity.load(event.params.creator.toHexString());
+  if (creatorEnt != null) {
+    creatorEnt.totalDownVote = creatorEnt.totalDownVote.plus(
       new BigInt(1)
     );
-    creatorEntity.creditScore = creatorEntity.totalUpVote
+    creatorEnt.creditScore = creatorEnt.totalUpVote
       .times(coefficientUp)
-      .minus(creatorEntity.totalDownVote.times(coefficientDown));
+      .minus(creatorEnt.totalDownVote.times(coefficientDown));
   }
 }
